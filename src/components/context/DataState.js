33c3de3aa0c30c95f93e60acb9dd42a76a9c8595
BartekSkr <bartoszskorka86@gmail.com>
2021-08-03 13:25:10 +0100

    added character search by name

--- src/components/context/DataState.js
+++ src/components/context/DataState.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import DataContext from './dataContext'
 
 export const DataState = ({ children }) => {
-  const [character, setCharacter] = useState([])
+  const [characters, setCharacters] = useState([])
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
@@ -10,30 +10,32 @@ export const DataState = ({ children }) => {
     // eslint-disable-next-line
   }, [])
 
-  //  getting data from apis (getData() & resPromise())
+  //  getting data from api
   const getData = (pageNumber) => {
     setLoading(true)
-    fetch(`https://swapi.dev/api/people/?page=${pageNumber}&format=json`)
+    fetch(`https://swapi.dev/api/people/?page=${pageNumber}`)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
         setLoading(false)
+        setCharacters(data.results)
+
+        console.log(data.results)
     })
   }
 
+  //  searching a character by name
   const searchCharacterByName = e => {
     if (e.key === 'Enter') {
       fetch(`https://swapi.dev/api/people/?search=${e.target.value}`)
         .then(response => response.json())
         .then(data => {
-          setCharacter(data)
           console.log(data)
         })
     }
   }
 
   return (
-    <DataContext.Provider value={{ loading, searchCharacterByName }}>
+    <DataContext.Provider value={{ loading, searchCharacterByName, characters }}>
       {children}
     </DataContext.Provider>
   )
