cda58e0dff84f1ec10fe609c4605f947ecdd4adf
BartekSkr <bartoszskorka86@gmail.com>
2021-08-04 18:00:34 +0100

    pagination component

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react'
+import React, { Fragment, useContext, useState } from 'react'
 import DataContext from '../context/dataContext'
+import './Pagination.css'
 
 export const Pagination = ({ postsPerPage, totalPosts }) => {
   const dataContext = useContext(DataContext)
   const { getData } = dataContext
+  const [currentPage, setCurrentPage] = useState(1)
   const pageNumbers = []
 
   // for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++){
@@ -14,17 +16,52 @@ export const Pagination = ({ postsPerPage, totalPosts }) => {
     pageNumbers.push(i)
   }
 
+  const handleOnClick = num => {
+    return function () {
+      getData(num)
+      setCurrentPage(num)
+      console.log('currentPage: ', currentPage)
+      console.log('number: ', num)
+    }
+  }
+
   return (
-    <div>
-      <ul>
+    <Fragment>
+      <ul id='pagination-ul'>
         {pageNumbers.map(number => (
-          <li key={number}>
-            <a onClick={() => getData(number)} href="/#">
+          // <li id='pagination-li' key={number}>
+          // <li id='pagination-li-chosen' key={currentPage}>
+          <li id='pagination-li' key={number}>
+            {/* <a onClick={() => getData(number)} href="/#"> */}
+            {/* <a id='pagination-a' */}
+            <a id={number === currentPage ? 'pagination-a-chosen' : 'pagination-a'}
+              // onClick={() => {
+              //   getData(number)
+              //   setCurrentPage(number)
+              //   console.log('currentPage: ', currentPage)
+              //   console.log('number: ', number)
+              // }}
+              onClick={handleOnClick(number)}
+              href="/#">
               {number}
             </a>
           </li>
         ))}
       </ul>
-    </div>
+    </Fragment>
   )
+
+  // return (
+  //   <div>
+  //     <ul>
+  //       {pageNumbers.map(number => (
+  //         <li key={number}>
+  //           <a onClick={() => getData(number)} href="/#">
+  //             {number}
+  //           </a>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // )
 }
