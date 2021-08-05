d613ce3031eaacebe885b385275722824d010f96
BartekSkr <bartoszskorka86@gmail.com>
2021-08-05 19:36:18 +0100

    character list animations & tooltips for buttons

--- src/components/context/DataState.js
+++ src/components/context/DataState.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react'
+import ReactTooltip from 'react-tooltip'
 import DataContext from './dataContext'
 
 export const DataState = ({ children }) => {
+  //  characters data from API
   const [characters, setCharacters] = useState([])
+  //  current data page
   const [currentPage, setCurrentPage] = useState(1)
+  //  state of a loading spinner
   const [loading, setLoading] = useState(false)
+  //  state of a button showing character's details
+  const [buttonKey, setButtonKey] = useState('')
 
   useEffect(() => {
     getData(1)
+    ReactTooltip.rebuild()
     // eslint-disable-next-line
   }, [])
 
   //  getting data from api
-  const getData = (pageNumber) => {
+  const getData = pageNumber => {
     setLoading(true)
     fetch(`https://swapi.dev/api/people/?page=${pageNumber}`)
       .then(response => response.json())
@@ -37,7 +44,7 @@ export const DataState = ({ children }) => {
   }
 
   return (
-    <DataContext.Provider value={{ loading, searchCharacterByName, characters, getData, currentPage }}>
+    <DataContext.Provider value={{ loading, searchCharacterByName, characters, getData, currentPage, buttonKey, setButtonKey }}>
       {children}
     </DataContext.Provider>
   )
--- src/components/pages/home/Characters.js
+++ src/components/pages/home/Characters.js
@@ -1,18 +1,17 @@
-import React, { Fragment, useContext, useState } from 'react'
+import React, { Fragment, useContext } from 'react'
 import './Characters.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { Search } from './Search'
 import { Spinner } from '../../layout/Spinner'
 import DataContext from '../../context/dataContext'
 import { Pagination } from '../../pagination/Pagination'
+import ReactTooltip from 'react-tooltip'
 
 export const Characters = () => {
   const dataContext = useContext(DataContext)
-  const { characters, loading } = dataContext
-  // const [currentPage, setCurrentPage] = useState(1)
-
-  // const paginate = (pageNumber) => {
-  //   setCurrentPage(pageNumber)
-  // }
+  const { characters, loading, buttonKey, setButtonKey } = dataContext
 
   if (loading) {
     return <Spinner />
@@ -24,20 +23,35 @@ export const Characters = () => {
         {characters.map((character, index) => (
           <div key={index} className='character'>
             <div className='character-info'>
-              <h3>{character.name}</h3>
-              <span><strong>Height: </strong><p>{character.height} cm</p></span>
-              <span><strong>Mass: </strong><p>{character.mass} kg</p></span>
-              <span><strong>Hair color: </strong><p>{character.hair_color}</p></span>
-              <span><strong>Birth year: </strong><p>{character.birth_year}</p></span>
-              <span><strong>Gender: </strong><p>{character.gender}</p></span>
+              <div className='character-info-header'>
+                <h3>{character.name}</h3>
+                <div>
+                  <button className='add-button' data-tip='Add to favourites'>
+                    <FontAwesomeIcon icon={faPlus} />
+                  </button>
+                  <button
+                    className='details-button'
+                    data-tip='Show details'
+                    onClick={() => {
+                      setButtonKey(character.created)
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faChevronDown} />
+                  </button>
+                </div>
+              </div>
+              <div className={buttonKey === character.created ? 'character-info-details-active' : 'character-info-details'}>
+                <span><strong>Height: </strong><p>{character.height} cm</p></span>
+                <span><strong>Mass: </strong><p>{character.mass} kg</p></span>
+                <span><strong>Hair color: </strong><p>{character.hair_color}</p></span>
+                <span><strong>Birth year: </strong><p>{character.birth_year}</p></span>
+                <span><strong>Gender: </strong><p>{character.gender}</p></span>
+              </div>
             </div>
-            <div className='character-button'>
-              <button className='add-button'>Add to favourites</button>
-            </div>
+            <ReactTooltip place='left' effect='solid' type='info' />
           </div>
         ))}
       </Fragment>
-        {/* <Pagination postsPerPage={characters.length} totalPosts={90} /> */}
         <Pagination />
     </Fragment>
     )
