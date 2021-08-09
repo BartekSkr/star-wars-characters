933a6299ff585080a4df609fe7d82aadd8306dd1
BartekSkr <bartoszskorka86@gmail.com>
2021-08-09 15:45:20 +0100

    page 404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar } from './components/layout/Navbar'
 import { DataState } from './components/context/DataState'
 import { Characters } from './components/pages/characters/Characters';
+import { PageNotFound } from './components/pages/404/404-page';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className='container'>
           <Switch>
             <Route exact path='/' component={Characters} />
+            <Route component={PageNotFound}/>
           </Switch>
         </div>
       </Router>
---
+++ src/components/pages/404/404-page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './404-page.css'
+import darthVader from '../../icons/darth-vader.ico'
+
+export const PageNotFound = () => {
+  return (
+    <div className='not-found-container'>
+      <img src={darthVader} alt="darth vader icon" />
+      <h2>ERROR</h2>
+      <div className='not-found-info'>
+        <h3>The page you are looking for, we cannot find!</h3>
+          <ul className='not-found-ul'>
+            <li>Not exist, page does.</li>
+            <li>The page changed its location.</li>
+            <li>Under construction, the website is. Underway, maintenance work is.</li>
+          </ul>
+      </div>
+    </div>
+  )
+}
