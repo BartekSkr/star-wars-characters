569a91563ca45876087f219810cde6ab9c79cf9e
BartekSkr <bartoszskorka86@gmail.com>
2021-09-01 14:57:47 +0100

    code cleanup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from './components/layout/Navbar'
 import { DataState } from './components/context/DataState'
 import { Characters } from './components/pages/characters/Characters';
 import { PageNotFound } from './components/pages/404/404-page';
+import { Favourites } from './components/pages/favourites/Favourites';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div className='container'>
           <Switch>
             <Route exact path='/' component={Characters} />
+            <Route exact path='/favourites' component={Favourites} />
             <Route component={PageNotFound}/>
           </Switch>
         </div>
