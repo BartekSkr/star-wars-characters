f5e6ddbce4fb6f49c1d9cecfba9de27d6965a755
BartekSkr <bartoszskorka86@gmail.com>
2021-09-02 15:57:42 +0100

    code cleanup

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,24 +1,5 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore } from 'redux'
 import { favouriteListReducer } from '../reducers/favouritesReducer'
-
-// export let store = createStore(favouriteList)
-
-// store.subscribe(() => console.log(store.getStore()))
-
-// store.dispatch(addToFavourites())
-
-// const initialState = {}
-// const middleware = [thunk]
-
-// export const store = createStore(
-//   favouriteListReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// )
 
 export const store = createStore(
   favouriteListReducer,
