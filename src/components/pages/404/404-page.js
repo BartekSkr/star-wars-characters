80ac8d2cd7a33db0c36d790aa425e8c9ca5a3694
BartekSkr <bartoszskorka86@gmail.com>
2021-10-04 21:28:09 +0100

    dark theme bug fixes

--- src/components/pages/404/404-page.js
+++ src/components/pages/404/404-page.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './404-page.css'
-import darthVader from '../../icons/darth-vader.png'
+import darthVader from '../../icons/darth-vader.svg'
+import darthVader2 from '../../icons/darth-vader-2.svg'
+import { useContext } from 'react'
+import DataContext from '../../context/dataContext'
 
 export const PageNotFound = () => {
+  const dataContext = useContext(DataContext)
+  const { myTheme } = dataContext
+
   return (
     <div className='not-found-container'>
-      <img src={darthVader} alt="darth vader icon" />
+      {/* <img src={darthVader} alt="darth vader icon" /> */}
+      <img src={myTheme === 'light' ? darthVader : darthVader2} alt="darth vader icon" />
       <h2>ERROR</h2>
       <div className='not-found-info'>
         <h3>The page you are looking for, we cannot find!</h3>
