598ff5f4dec84a69665cdfe0877c06f1893654a9
BartekSkr <bartoszskorka86@gmail.com>
2021-11-09 14:53:47 +0100

    code cleanup

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,7 +1,5 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { favouriteListReducer } from '../reducers/favouritesReducer'
 
-export const store = createStore(
-  favouriteListReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+export const store = createStore(favouriteListReducer, composeWithDevTools())
