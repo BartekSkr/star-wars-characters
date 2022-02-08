fe4426e2cc82dedcd3d1379eba18fd88c661ae7c
Bartek <bartoszskorka86@gmail.com>
2022-02-08 10:51:33 +0100

    reducer

--- src/app/reducers/favouritesReducer.js
+++ src/app/reducers/favouritesReducer.js
@@ -1,30 +1,36 @@
-import { ADD_TO_FAVOURITES, DELETE_FAVOURITE_LIST, DELETE_FROM_FAVOURITES } from '../actions/types'
+import {
+  ADD_TO_FAVOURITES,
+  DELETE_FAVOURITE_LIST,
+  DELETE_FROM_FAVOURITES,
+} from '../actions/types';
 
-const favouritesList = JSON.parse(localStorage.getItem('favourites') || '[]')
+const favouritesList = JSON.parse(localStorage.getItem('favourites') || '[]');
 
 export const INITIAL_STATE = {
   listName: 'Favourite characters',
-  list: favouritesList
-}
+  list: favouritesList,
+};
 
 export const favouriteListReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ADD_TO_FAVOURITES:
       return {
         ...state,
-        list: [...state.list, action.payload]
-      }
+        list: [...state.list, action.payload],
+      };
     case DELETE_FROM_FAVOURITES:
       return {
         ...state,
-        list: state.list.filter(character => character.name !== action.payload.name)
-      }
+        list: state.list.filter(
+          (character) => character.name !== action.payload.name
+        ),
+      };
     case DELETE_FAVOURITE_LIST:
       return {
         ...state,
-        list: []
-      }
+        list: [],
+      };
     default:
-      return state
+      return state;
   }
-}
+};
