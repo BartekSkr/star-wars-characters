38162a3722317cceedcffff093668f88396e459e
Bartosz Skórka <bartosz.skorka@monogo.pl>
2022-05-20 14:46:15 +0200

    changes in toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -48,7 +48,7 @@ const App: React.FC<AppProps> = ({
       </Router>
       <ToastContainer
         position='bottom-center'
-        hideProgressBar={true}
+        hideProgressBar={false}
         pauseOnHover={false}
         pauseOnFocusLoss={false}
         draggable={false}
