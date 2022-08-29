9844b5249d7b143431b27955c7c29b18bc2341ef
Bartosz Skórka <bartosz.skorka@monogo.pl>
2022-08-29 16:54:48 +0200

    tailwind installation

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,tsx}'],
+  theme: {
+    colors: {
+      'default-color': 'rgb(255, 255, 0)',
+    },
+    borderWidth: {
+      3: '3px',
+      4: '4px',
+    },
+  },
+  plugins: [],
+};
